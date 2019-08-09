3875792435ff090bb4035bcdbb3488b90c996409
Giorgio Mula <giorgio.mula@tomtom.com>
2019-08-09 16:40:06 +0200

    Improved key event presentation with action and modifier

--- app/src/main/java/com/example/keylogger/MainActivity.java
+++ app/src/main/java/com/example/keylogger/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.keylogger;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Locale;
@@ -14,27 +13,42 @@ import static android.view.KeyEvent.*;
 
 public class MainActivity extends Activity {
     private TextView mKeyReceivedTextView;
+    private TextView mKeyReceivedTextViewModifier;
+    private TextView mKeyReceivedTextViewEvent;
+    private CountDownTimer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main_activity);
         mKeyReceivedTextView = findViewById(R.id.textView);
+        mKeyReceivedTextViewModifier = findViewById(R.id.textViewModifier);
+        mKeyReceivedTextViewEvent = findViewById(R.id.textViewEvent);
+        timer = new CountDownTimer(2000, 2000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
 
+            }
+
+            @Override
+            public void onFinish() {
+                mKeyReceivedTextView.setText(R.string.text_description);
+                mKeyReceivedTextViewModifier.setText("");
+                mKeyReceivedTextViewEvent.setText("");
+            }
+        };
     }
 
     @Override
     public boolean dispatchKeyEvent(KeyEvent event) {
         printKey(event);
-        // Exit from application
-        if ((event.getKeyCode() == KEYCODE_BACK) && event.isLongPress()) {
-            return super.dispatchKeyEvent(event);
-        }
+        timer.start();
         return true;
     }
 
     private void printKey(KeyEvent event) {
         int keycode = event.getKeyCode();
+
         String codeName;
         switch (keycode){
             case KEYCODE_DPAD_CENTER:
@@ -66,11 +80,14 @@ public class MainActivity extends Activity {
                 codeName = String.format(Locale.ENGLISH, "KEYCODE: %d", keycode);
                 break;
         }
-        if (event.isLongPress()) {
-            codeName += " - LONG PRESS";
-        }
+
+        String modifier = event.isLongPress()? "LONG PRESS": "";
+        String action = event.getAction() == ACTION_UP? "UP": "DOWN";
 
         mKeyReceivedTextView.setText(codeName);
+        mKeyReceivedTextViewModifier.setText(modifier);
+        mKeyReceivedTextViewEvent.setText(action);
+
     }
 
     public void onExitButtonClick(View view) {
